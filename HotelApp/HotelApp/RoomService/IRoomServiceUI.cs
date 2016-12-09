d1706ca00bc551378d0eecc6f1beeb4b3d8792be
Pepejson <pepejson@gmail.com>
2016-12-09 16:49:27 +0100

    [RoomService] Początek tak bardzo. Dodanie podstawowych klas

---
+++ HotelApp/HotelApp/RoomService/IRoomServiceUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.RoomService
+{
+    interface IRoomServiceUI
+    {
+        bool acceptReservation();
+        bool editReservation();
+        bool editRoom();
+        String notification();
+    }
+}
