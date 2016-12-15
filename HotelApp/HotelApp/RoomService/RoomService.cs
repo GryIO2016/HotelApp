ab97ea719841845e8181042c6b80062854377cd8
Leonard Oganiesian <187203@edu.p.lodz.pl>
2016-12-15 13:46:15 +0100

    Metody sprawdzające status pokoi

--- HotelApp/HotelApp/RoomService/RoomService.cs
+++ HotelApp/HotelApp/RoomService/RoomService.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelApp.Database;
 
 namespace HotelApp.RoomService
 {
-    class RoomService
+    public class RoomService
     {
+        public List<Room> roomsToCleanList (List<Room> roomsList)
+        {
+            List<Room> roomsToClean = new List<Room>();
+            foreach (Room room in roomsList)
+            {
+                if (room.RoomStatus == EnumHelper.Status.ToClean)
+                {
+                    roomsToClean.Add(room);
+                }
+            }
+
+            return roomsToClean;
+        }
+
+        public bool checkRoomAvailability(Room room)
+        {
+            if (room.RoomStatus == EnumHelper.Status.Free)
+                return true;
+            else
+                return false;
+        }
+
+        public List<Room> freeRoomsList (List<Room> roomsList)
+        {
+            List<Room> freeRooms = new List<Room>();
+            foreach (Room room in roomsList)
+            {
+                if (room.RoomStatus == EnumHelper.Status.Free)
+                {
+                    freeRooms.Add(room);
+                }
+
+            }
+            return freeRooms;
+       }
     }
 }
