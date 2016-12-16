7f6164cd692533d2e2beb45cf6ad824b36f923e8
adrianw1994 <189809@edu.p.lodz.pl>
2016-12-16 20:03:34 +0100

    Dodanie obsługi wyjatków w DBManagement

--- HotelApp/HotelApp/Database/DBManagement.cs
+++ HotelApp/HotelApp/Database/DBManagement.cs
@@ -18,8 +18,8 @@ namespace HotelApp.Database
                 r.check_in = reservation.CheckInDate;
                 r.check_out = reservation.CheckOutDate;
                 r.confirmed = reservation.Confirmed;
-                r.paid = (decimal) reservation.Paid;
-                r.total_price = (decimal) reservation.TotalPrice;
+                r.paid = (decimal)reservation.Paid;
+                r.total_price = (decimal)reservation.TotalPrice;
                 r.user_id = reservation.User.Id;
                 r.canceled = reservation.Canceled;
 
@@ -37,45 +37,59 @@ namespace HotelApp.Database
                 Conn.reservations.Add(r);
                 Conn.SaveChanges();
             }
+
         }
+
 
         public List<Room> getFreeRooms(DateTime startDate, DateTime endTime)
         {
             List<Room> fRrooms = new List<Room>();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                //rezerwacje, które znajdują się w danym przedziale lub czesciowo na niego nachodzą
-                var find = from d in Conn.reservations where (
-                        (d.check_in < startDate & d.check_out > endTime) ||
-                        (d.check_in > startDate & d.check_in < endTime) ||
-                        (d.check_out > startDate & d.check_out < endTime)
-                    ) select d;
-
-                // pokoje zarezerwowane w tym czasie
-                List<int> roomsid = new List<int>();
-                foreach (var f in find)
+                using (var Conn = new IO2017Entities())
                 {
-                    foreach (rooms r in f.rooms)
+                    //rezerwacje, które znajdują się w danym przedziale lub czesciowo na niego nachodzą
+                    var find = from d in Conn.reservations
+                               where (
+                                    (d.check_in < startDate & d.check_out > endTime) ||
+                                    (d.check_in > startDate & d.check_in < endTime) ||
+                                    (d.check_out > startDate & d.check_out < endTime)
+                                    )
+                               select d;
+
+                    // pokoje zarezerwowane w tym czasie
+                    List<int> roomsid = new List<int>();
+                    foreach (var f in find)
                     {
-                        roomsid.Add(r.room_id);
+                        foreach (rooms r in f.rooms)
+                        {
+                            roomsid.Add(r.room_id);
+                        }
+                    }
+
+                    // wolne pokoje
+                    var free = from q in Conn.rooms where !(roomsid.Contains(q.room_id)) select q;
+
+                    foreach (var qq in free)
+                    {
+                        fRrooms.Add(new Room(qq.room_id, qq.number, (double)qq.price, qq.smoking, qq.pets, (EnumHelper.BedType)qq.bed_type, (EnumHelper.Status)qq.room_status));
                     }
                 }
-
-                // wolne pokoje
-                var free = from q in Conn.rooms where !(roomsid.Contains(q.room_id)) select q;
-
-                foreach (var qq in free)
-                {
-                    fRrooms.Add(new Room(qq.room_id, qq.number, (double)qq.price, qq.smoking, qq.pets, (EnumHelper.BedType)qq.bed_type, (EnumHelper.Status)qq.room_status));
-                }
-
             }
+            catch
+            {
+                return null;
+            }
+
+
+
             return fRrooms;
         }
 
         public void editReservation(Reservation oldReservation, Reservation newReservation)
         {
+
             using (var Conn = new IO2017Entities())
             {
                 reservations reservation = Conn.reservations.Find(oldReservation.Id);
@@ -110,29 +124,37 @@ namespace HotelApp.Database
         {
             List<Reservation> reservations = new List<Reservation>();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                var find = Conn.reservations
-                    .Where(b => b.check_in >= startDate & b.check_out <= endDate);
-
-                foreach (var reservation in find)
+                using (var Conn = new IO2017Entities())
                 {
-                    List<Room> rooms = new List<Room>();
-                    foreach (var r in reservation.rooms)
+                    var find = Conn.reservations
+                        .Where(b => b.check_in >= startDate & b.check_out <= endDate);
+
+                    foreach (var reservation in find)
                     {
-                        Room room = new Room(r.room_id, r.number, (double)r.price, r.smoking, r.pets, (EnumHelper.BedType)r.bed_type, (EnumHelper.Status)r.room_status);
-                        rooms.Add(room);
+                        List<Room> rooms = new List<Room>();
+                        foreach (var r in reservation.rooms)
+                        {
+                            Room room = new Room(r.room_id, r.number, (double)r.price, r.smoking, r.pets, (EnumHelper.BedType)r.bed_type, (EnumHelper.Status)r.room_status);
+                            rooms.Add(room);
+                        }
+
+                        var u = Conn.users
+                            .Where(b => b.user_id == reservation.user_id)
+                            .FirstOrDefault();
+
+                        User user = new User(u.user_id, u.first_name, u.last_name, (DateTime)u.birth_date, u.phone_number, u.email, u.password, u.pesel, (EnumHelper.Role)u.role);
+
+                        reservations.Add(new Reservation(reservation.reservation_id, user, reservation.check_in, reservation.check_out, reservation.confirmed, (double)reservation.paid, (double)reservation.total_price, rooms, reservation.canceled));
                     }
-
-                    var u = Conn.users
-                        .Where(b => b.user_id == reservation.user_id)
-                        .FirstOrDefault();
-
-                    User user = new User(u.user_id, u.first_name, u.last_name, (DateTime)u.birth_date, u.phone_number, u.email, u.password, u.pesel, (EnumHelper.Role)u.role);
-
-                    reservations.Add(new Reservation(reservation.reservation_id, user, reservation.check_in, reservation.check_out, reservation.confirmed, (double)reservation.paid , (double)reservation.total_price, rooms, reservation.canceled));
                 }
             }
+            catch
+            {
+                return null;
+            }
+
 
             return reservations;
         }
@@ -141,22 +163,30 @@ namespace HotelApp.Database
         {
             List<Reservation> reservations = new List<Reservation>();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                var find = Conn.reservations
-                    .Where(b => b.user_id == user.Id);
-
-                foreach (var reservation in find)
+                using (var Conn = new IO2017Entities())
                 {
-                    List<Room> rooms = new List<Room>();
-                    foreach (var r in reservation.rooms)
+                    var find = Conn.reservations
+                        .Where(b => b.user_id == user.Id);
+
+                    foreach (var reservation in find)
                     {
-                        Room room = new Room(r.room_id, r.number, (double)r.price, r.smoking, r.pets, (EnumHelper.BedType)r.bed_type, (EnumHelper.Status)r.room_status);
-                        rooms.Add(room);
+                        List<Room> rooms = new List<Room>();
+                        foreach (var r in reservation.rooms)
+                        {
+                            Room room = new Room(r.room_id, r.number, (double)r.price, r.smoking, r.pets, (EnumHelper.BedType)r.bed_type, (EnumHelper.Status)r.room_status);
+                            rooms.Add(room);
+                        }
+                        reservations.Add(new Reservation(reservation.reservation_id, user, reservation.check_in, reservation.check_out, reservation.confirmed, (double)reservation.paid, (double)reservation.total_price, rooms, reservation.canceled));
                     }
-                    reservations.Add(new Reservation(reservation.reservation_id, user, reservation.check_in, reservation.check_out, reservation.confirmed, (double)reservation.paid, (double)reservation.total_price, rooms, reservation.canceled));
                 }
             }
+            catch
+            {
+                return null;
+            }
+
 
             return reservations;
         }
@@ -166,6 +196,7 @@ namespace HotelApp.Database
 
         public void addUser(User user)
         {
+
             using (var Conn = new IO2017Entities())
             {
                 users u = new users();
@@ -184,6 +215,14 @@ namespace HotelApp.Database
 
         public void editUser(User oldUser, User newUser)
         {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
             using (var Conn = new IO2017Entities())
             {
                 users u = Conn.users.Find(oldUser.Id);
@@ -207,22 +246,30 @@ namespace HotelApp.Database
         {
             User user = new User();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                var u = Conn.users
-                .Where(b => b.email == email)
-                .FirstOrDefault();
+                using (var Conn = new IO2017Entities())
+                {
+                    var u = Conn.users
+                    .Where(b => b.email == email)
+                    .FirstOrDefault();
 
-                user.Id = u.user_id;
-                user.Name = u.first_name;
-                user.LastName = u.last_name;
-                user.BirthDate = (DateTime)u.birth_date;
-                user.Phone = u.phone_number;
-                user.Email = u.email;
-                user.Password = u.password;
-                user.Pesel = u.pesel;
-                user.Role = (EnumHelper.Role)u.role;
+                    user.Id = u.user_id;
+                    user.Name = u.first_name;
+                    user.LastName = u.last_name;
+                    user.BirthDate = (DateTime)u.birth_date;
+                    user.Phone = u.phone_number;
+                    user.Email = u.email;
+                    user.Password = u.password;
+                    user.Pesel = u.pesel;
+                    user.Role = (EnumHelper.Role)u.role;
+                }
             }
+            catch
+            {
+                return null;
+            }
+
             return user;
 
         }
@@ -231,21 +278,29 @@ namespace HotelApp.Database
         {
             User user = new User();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                var u = Conn.users
-                .Where(b => b.email == email && b.password == password)
-                .FirstOrDefault();
+                using (var Conn = new IO2017Entities())
+                {
+                    var u = Conn.users
+                    .Where(b => b.email == email && b.password == password)
+                    .FirstOrDefault();
 
-                user.Name = u.first_name;
-                user.LastName = u.last_name;
-                user.BirthDate = (DateTime)u.birth_date;
-                user.Phone = u.phone_number;
-                user.Email = u.email;
-                user.Password = u.password;
-                user.Pesel = u.pesel;
-                user.Role = (EnumHelper.Role)u.role;
+                    user.Name = u.first_name;
+                    user.LastName = u.last_name;
+                    user.BirthDate = (DateTime)u.birth_date;
+                    user.Phone = u.phone_number;
+                    user.Email = u.email;
+                    user.Password = u.password;
+                    user.Pesel = u.pesel;
+                    user.Role = (EnumHelper.Role)u.role;
+                }
             }
+            catch
+            {
+                return null;
+            }
+
 
             return user;
         }
@@ -257,32 +312,41 @@ namespace HotelApp.Database
         {
             Room room = new Room();
 
-            using (var Conn = new IO2017Entities())
+            try
             {
-                var r = Conn.rooms
-                .Where(b => b.room_id == id)
-                .FirstOrDefault();
+                using (var Conn = new IO2017Entities())
+                {
+                    var r = Conn.rooms
+                    .Where(b => b.room_id == id)
+                    .FirstOrDefault();
 
-                room.Id = r.room_id;
-                room.Number = r.number;
-                room.Price = (double)r.price;
-                room.Smoking = r.smoking;
-                room.Pets = r.pets;
-                room.BedType = (EnumHelper.BedType)r.bed_type;
-                room.RoomStatus = (EnumHelper.Status)r.room_status;
+                    room.Id = r.room_id;
+                    room.Number = r.number;
+                    room.Price = (double)r.price;
+                    room.Smoking = r.smoking;
+                    room.Pets = r.pets;
+                    room.BedType = (EnumHelper.BedType)r.bed_type;
+                    room.RoomStatus = (EnumHelper.Status)r.room_status;
+                }
             }
+            catch
+            {
+                return null;
+            }
+
             return room;
         }
 
         public void editRoom(Room oldRoom, Room newRoom)
         {
+
             using (var Conn = new IO2017Entities())
             {
                 rooms r = Conn.rooms.Find(oldRoom.Id);
                 if (r != null)
                 {
                     r.number = newRoom.Number;
-                    
+
                     r.price = (decimal)newRoom.Price;
                     r.smoking = newRoom.Smoking;
                     r.pets = newRoom.Pets;
