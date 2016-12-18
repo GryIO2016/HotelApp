2287eefaa91a7f16cb23cb2e4328892cb28dcddb
kubaholys <jaxaaaaa@gmail.com>
2016-12-19 00:34:37 +0100

    [Logowanie] Poprawa i porządki
    Usunięto niepotrzebne rzeczy (okna, klasy niezgodne z dokumentacją).
    Dodano: ILoginUI, Logger (bez metody RegisterUser()).

---
+++ HotelApp/HotelApp/LoginModule/ILoginUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelApp.Database;
+
+namespace HotelApp.LoginModule
+{
+    interface ILoginUI
+    {
+        User LogIn(string email, string password);
+        int RegisterUser(string name, string lastName, DateTime birthDate, string phone, string email, string password, string pesel, EnumHelper.Role role);
+        bool Exists(string email);
+    }
+}
---
+++ HotelApp/HotelApp/LoginModule/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelApp.Database;
+
+namespace HotelApp.LoginModule
+{
+    static class Logger : ILoginUI
+    {
+        private DBManagement dataBase = new DBManagement();
+        bool Exists(string email)
+        {
+            if (dataBase.findUser(email)) return true;
+            else return false;
+        }
+        User LogIn(string email, string password)
+        {
+            User user = dataBase.findUser(email, password);
+            if (user)
+            {
+                return user;
+            }
+            else return user;
+        }
+        bool RegisterUser(string name, string lastName, DateTime birthDate, string phone, string email, string password, string pesel, EnumHelper.Role role)
+        {
+            if (Exists(email))
+            {
+                return true;    //this means that there already IS user with the given e-mail address
+            }
+
+        }
+    }
+}
